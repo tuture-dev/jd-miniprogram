5bbd28e5a60bbc1f3f1b60adbf9459aaef76eabf
Tom Huang <huan1043269994@gmail.com>
2020-04-20 11:28:30 +0800

    使用 React Hooks 并处理事件

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,13 +1,21 @@
-import Taro from "@tarojs/taro";
+import Taro, { useState } from "@tarojs/taro";
 import { View, Button, Textarea } from "@tarojs/components";
 import "./index.scss";
 
 export default function Index() {
+  const [post, setPost] = useState("");
+
+  function handleChange(e) {
+    setPost(e.target.value);
+  }
+
   return (
     <View className="index">
       <Textarea
         placeholder="撰写优质教程..."
         className="post-input"
+        value={post}
+        onInput={handleChange}
         autoHeight
       />
       <Button className="post-button">发表</Button>
